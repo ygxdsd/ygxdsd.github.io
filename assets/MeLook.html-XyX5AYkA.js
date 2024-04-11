04e37ac81b24ee924c56cc2b8de3c01a3e90dd35
ygxdsd <76094322+ygxdsd@users.noreply.github.com>
2024-04-11 12:59:33 +0000

    Deploying to gh-pages from @ ygxdsd/ygxdsd.github.io@55503ab73916a86de8c1d30bbf7700abe8f80941 🚀

---
+++ assets/MeLook.html-XyX5AYkA.js
@@ -0,0 +1,36 @@
+import{_ as c}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as v,o as r,c as o,d as a,f as m,w as n,a as d,b as s,e as l}from"./app-xS5i7JKJ.js";const u={},b=d("p",null,[d("code",null,"more"),s(" 网站内的博客编写丰富化")],-1),p=l(`<h3 id="选项卡" tabindex="-1"><a class="header-anchor" href="#选项卡" aria-hidden="true">#</a> 选项卡</h3><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs
+
+@tab 标题 1
+
+<span class="token comment">&lt;!-- tab 1 内容 --&gt;</span>
+
+
+@tab 标题 2
+
+<span class="token comment">&lt;!-- tab 2 内容 --&gt;</span>
+
+@tab:active 标题 3
+
+<span class="token comment">&lt;!-- tab 3 将会被默认激活 --&gt;</span>
+
+<span class="token comment">&lt;!-- tab 3 内容 --&gt;</span>
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),h=d("p",null,"这是标题1的内容",-1),_=l(`<h3 id="重要提醒" tabindex="-1"><a class="header-anchor" href="#重要提醒" aria-hidden="true">#</a> 重要提醒</h3><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: important 自定义重要
+:::
+
+::: info 自定义信息
+:::
+
+::: note 自定义注释
+:::
+
+::: tip 自定义提示
+:::
+
+::: warning 自定义警告
+:::
+
+::: caution 自定义危险
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="hint-container important"><p class="hint-container-title">自定义重要</p></div><div class="hint-container info"><p class="hint-container-title">自定义信息</p></div><div class="hint-container note"><p class="hint-container-title">自定义注释</p></div><div class="hint-container tip"><p class="hint-container-title">自定义提示</p></div><div class="hint-container warning"><p class="hint-container-title">自定义警告</p></div><div class="hint-container caution"><p class="hint-container-title">自定义危险</p></div>`,8);function g(f,k){const t=v("Tabs");return r(),o("div",null,[b,a(" more "),p,m(t,{id:"8",data:[{id:"标题 1"},{id:"标题 2"},{id:"标题 3"}]},{title0:n(({value:i,isActive:e})=>[s("标题 1")]),title1:n(({value:i,isActive:e})=>[s("标题 2")]),title2:n(({value:i,isActive:e})=>[s("标题 3")]),tab0:n(({value:i,isActive:e})=>[a(" tab 1 内容 "),h]),tab1:n(({value:i,isActive:e})=>[a(" tab 2 内容 ")]),tab2:n(({value:i,isActive:e})=>[a(" tab 3 将会被默认激活 "),a(" tab 3 内容 ")]),_:1}),_])}const A=c(u,[["render",g],["__file","MeLook.html.vue"]]);export{A as default};
